f80f7be7c24410ad42e9b536dbd1656ed81c30c7
priyanshu sharma <sharmapriyanshu0204@gmail.com>
2024-12-29 08:25:04 +0530

    Car_Rental_System

---
+++ src/carrentalsystem/Car.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package carrentalsystem;
+
+/**
+ *
+ * @author psc
+ */
+class Car {
+
+    Car(String c001, String toyota, String hilux, double par) {
+    }
+
+    boolean isAvailable() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    double getBasePricePerDay(int rentalDays) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    String getCarId() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
